1d74f6423db4b95b8098b17ed3b28e4f2ae42a22
y3046308 <y3046308@gmail.com>
2012-07-16 01:41:32 +0000

    ㅁ

---
+++ office.cc
@@ -0,0 +1,54 @@
+#include "office.h"
+#include "PRNG.h"
+
+/*
+  purpose: constructor
+  precondition: printer
+  postcondition: None
+*/
+WATCardOffice::WATCardOffice( Printer &prt )
+{
+    office_printer = &prt;
+    printer->print(Printer::WATCardOffice, 'S');                          
+}
+
+/*
+  purpose: destructor
+  precondition: printer
+  postcondition: none
+*/
+WATCardOffice::~WATCardOffice( Printer &prt )
+{
+    office_printer = NULL;
+    printer->print(Printer::WATCardOffice, 'F');                          
+}
+
+/*
+  purpose: create a new watcard
+  precondition: unsigned int, unsigned int
+  postcondition: WATcard
+*/
+WATCard *WATCardOffice::create( unsigned int id, unsigned int amount )
+{
+        WATcard *new_card = new WATcard;
+        WATcard->balance = amount;
+        printer->print(Printer::WATCardOffice, 'C', id, amount);
+        return new_card;       
+}
+
+/*
+  purpose: transfer money to another watcard
+  precondition: unsigned int, unsigned int, WATcard
+  postcondition: NONE
+*/
+void WATCardOffice::transfer( unsigned int id, unsigned int amount, WATCard &card )
+{
+     int rand = prng(1,4); // pick a number b/w 1 to 4
+     
+     if (rand == 1)        // if random number is 1, 
+     {                     // There is 1/4 change to pick 1 from 1-4
+        amount /= 2;
+     }
+     card.addfund(amount);
+     printer->print(Printer::WATCardOffice, 'T', id, amount);
+}
