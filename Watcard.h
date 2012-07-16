86727fe4903d5cb0be555ef533df9876533c6e60
y3046308 <y3046308@gmail.com>
2012-07-16 01:42:50 +0000

    ㅁ

--- Watcard.h
+++ Watcard.h
@@ -1,16 +1,13 @@
 #ifndef __WATCARD_H__
 #define __WATCARD_H__
-#include <map>
 
 class WATCard {
-      protected:
-                unsigned int id, balance;
-                stu_list lst;
+      private:
+                unsigned int balance;
       public:
-             typedef map<unsigned int, unsigned int> stu_list;
              WATCard();
-             void add_fund(unsigned int account_id, unsigned int amount);
-             void sub_fund(unsigned int account_id, unsigned int amount);
+             void add_fund(unsigned int amount);
+             void sub_fund(unsigned int amount);
 };
 
 #endif
