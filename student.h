418a2f34229ae2e94e4509e32101e8d745b3b48a
y3046308 <y3046308@gmail.com>
2012-07-18 19:06:35 +0000

    aa

--- student.h
+++ student.h
@@ -1,6 +1,20 @@
-#ifndef __FLAVOUR__
-#define __FLAVOUR__
+#ifndef __STUDENT_H__
+#define __STUDENT_H__
+#include "printer.h"
+#include "nameserver.h"
+#inlcude "office.h"
+#include "flavour.h"
 
-enum Flavours {BlackCherry, CreamSoda, RootBeer, JazzLime};
+class Student {
+      unsigned int student_id, how_many, flavour, how_many_bought ;
+      WATcard *myCard;
+      Printer *student_printer;
+      Nameserver *student_server;
+      WATCardOffice *student_office;
+      VendingMachine *curr;
+  public:
+        Student( Printer &prt, NameServer &nameServer, WATCardOffice &cardOffice, unsigned int id, unsigned int maxPurchases );
+        bool action();
+};
 
 #endif
