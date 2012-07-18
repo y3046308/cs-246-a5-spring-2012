8628b8a9961cc6b9168bb2e991a4b67a9090ae03
y3046308 <y3046308@gmail.com>
2012-07-18 19:33:37 +0000

    a

--- student.cc
+++ student.cc
@@ -1,55 +1,76 @@
 #include "student.h"
 #include "PRNG.h"
+#include "flavour.h"
 
 PRNG prng;
 
 Student::Student( Printer &prt, NameServer &nameServer, WATCardOffice &cardOffice, 
                   unsigned int id, unsigned int maxPurchases)
 {
+    student_printer = &prt;
+    student_server = &nameserver;
+    student_office = &cardOffice;
     student_id = id;      
     how_many = prng(1, MaxPurchases);  
-    flavour = prng(3); 
+    how_many_bought = 0 ;
+    int temp = prng(3);
+      
+    if (temp == 0)
+    {
+       student_flavour = BlackCherry;
+    }  
+    else if (temp == 1)
+    {
+       student_flavour = CreamSoda;
+    }  
+    else if (temp == 2)
+    {
+       student_flavour = RootBeer;
+    }   
+    else
+    {
+        student_flavour = JazzLime;
+    }
+    myCard = new WATCard;
+    curr = student_server->getMachine(student_id);
+    
     transfer(id, 5, myCard);         
-	
-	if ( vending maching destroys a students WATCard ){
-		create a new WATCard with $0 balance and transferring the same initial $5 to the card with WATCardOffice
-	}                
-	
-	else if ( vending machine delivered a bottle of soda){
-		student drinks it and returns true
-	}
-	
-	else { (check WATCard for insufficient funds to purchase a bottle of soda)
-		if ( there are insufficient funds){
-			a student transfers $3 to their WATCard from the WATCard office until there is
-			sufficient funds to buy at least one soda and returns true
-			
-			
-			
-		}
-				
-	}
-	
-		
+    
+    student_printer->print(Printer::Student, student_id, 'S', flavour, how_many);    
+    student_printer->print(Printer::Student, student_id, 'V', curr->getId());
 }
 
+Student::~Student()
+{
+    student_printer = NULL;
+    student_server = NULL;
+    student_office = NULL;
+    student_id = 0;
+    how_many = 0;
+    how_many_bought = 0 ;
+    student_flavour = NULL;
+    delete myCard;
+    curr = NULL;
+    student_printer->print(Printer::Student, student_id, 'F');
+}
 
-
-/*
-When a student has purchased all the soda initially selected, then action returns false. 
-
-A student then attempts to buy a bottle of soda from the vending machine.
- - If the vending machine destroys a student’s WATCard, the student must create a new one by creating a WATCard (dynamically)
- with $0 balance and transferring the same initial $5 to the card via the WATCardOffice. 
-	
- - If the vending machine delivered a bottle of soda, the student drinks it and returns true.
-
- - Otherwise, a student checks their WATCard for insufficient funds (less than VendingMachine::cost()) to purchase a bottle of
- soda. 
-	
-      - If there is insufficient funds, a student transfers $3 to their WATCard from the WATCard office until there is 
-	sufficient funds to buy at least one soda and returns true; 
-      - the next time action is called, the student attempts to buy a soda with the new money. If the vending machine is out
-        of the student’s favourite flavour, the student must obtain a new vending machine from the name server and return 
-        true; the next time action is called, the student attempts to buy a soda at the new vending machine.
-*/
+bool Student::action()
+{
+    if (how_many_bought == 0)
+    {
+        return false;
+    }
+    
+    while (
+    
+    VendingMachine::Status stock = curr->buy(student_flavour, myCard);
+    
+    if (stock == 
+    
+    int rand_broken = prng(1,10);
+    
+    if (rand_broken == 1)
+    {
+    
+    }
+}
