f1e273d342748ec2aea05cb5b00575bdf7379691
y3046308 <y3046308@gmail.com>
2012-07-20 01:12:04 +0000

    a

--- flavour.h
+++ flavour.h
@@ -1,6 +1,6 @@
 #ifndef __FLAVOUR__
 #define __FLAVOUR__
 
-enum Flavours {BlackCherry, CreamSoda, RootBeer, JazzLime};
+enum Flavours {BlackCherry = 0, CreamSoda = 1, RootBeer = 2, JazzLime = 3};
 
 #endif
