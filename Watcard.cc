b4a9c65cbeeaeb4dee51948d5a414be7f5f189ed
y3046308 <y3046308@gmail.com>
2012-07-20 18:43:41 +0000

    aa

--- Watcard.cc
+++ Watcard.cc
@@ -1,20 +1,40 @@
 #include "Watcard.h"
 
+/*
+  purpose: constructor that initializes WATCard with a balance of 0
+  precondition: NONE
+  postcondition: WATCard
+*/
 WATCard::WATCard()
 {
-    balanced = 0;                 
+    balanced = 0; // set balance to 0
 }
 
-unsigned int WATCard::curr_balance()
-{
-    return balance;        
-}
-
+/*
+  purpose: add money to current watcard balance
+  precondition: unsigned int
+  postcondition: none
+*/
 void WATCard::add_fund(unsigned int amount)
 {
      balance += amount;
 }
 
+/*
+  purpose: display current balance in watcard
+  precondition: none
+  postcondition: unsigned int
+*/
+unsigned int WATCard::curr_balance()
+{
+    return balance;        
+}
+
+/*
+  purpose: subtract money to current watcard balance
+  precondition: unsigned int
+  postcondition: none
+*/
 void WATCard::sub_fund(unsigned int amount)
 {
     balance -= amount; 
