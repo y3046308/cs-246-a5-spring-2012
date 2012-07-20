a2751316c28cce3d817ae479e4cd9a66334bad70
y3046308 <y3046308@gmail.com>
2012-07-20 18:46:23 +0000

    aa

--- truck.h
+++ truck.h
@@ -9,6 +9,9 @@ class Truck {
              Printer *truck_printer;
              NameServer *truck_server;
              BottlingPlant *truck_Plant;
+             // numMac = number of Vending Machine
+             // maxStock = maximum number of stock available
+             // list[4] = a list of stock for each flavour
              unsigned int numMac, maxStock, list[4];
       public:
              Truck( Printer &prt, NameServer &nameServer, BottlingPlant &plant,
