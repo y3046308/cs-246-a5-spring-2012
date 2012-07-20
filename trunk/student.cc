85210f9ed3ef5214c07fea0f2e70717cd0ab444f
y3046308@gmail.com <y3046308@gmail.com@e4ebeee5-115f-8226-58e5-ea274c292319>
2012-07-20 01:11:29 +0000

    aa

--- trunk/student.cc
+++ trunk/student.cc
@@ -4,6 +4,11 @@
 
 PRNG prng;
 
+/*
+  purpose: initializes student
+  precondition: printer, Nameserver, WATCardOffice, unsigned int, unsigned int
+  postcondition: None
+*/
 Student::Student( Printer &prt, NameServer &nameServer, WATCardOffice &cardOffice, 
                   unsigned int id, unsigned int maxPurchases)
 {
@@ -13,26 +18,9 @@ Student::Student( Printer &prt, NameServer &nameServer, WATCardOffice &cardOffic
     student_id = id;      
     how_many = prng(1, MaxPurchases);  
     how_many_bought = 0 ;
-    int temp = prng(3);
-      
-    if (temp == 0)
-    {
-       student_flavour = BlackCherry;
-    }  
-    else if (temp == 1)
-    {
-       student_flavour = CreamSoda;
-    }  
-    else if (temp == 2)
-    {
-       student_flavour = RootBeer;
-    }   
-    else
-    {
-        student_flavour = JazzLime;
-    }
+    student_flavour = prng(3);
     myCard = new WATCard;
-    curr = student_server->getMachine(student_id);
+    curr_mach = student_server->getMachine(student_id);
     
     transfer(id, 5, myCard);         
     
@@ -50,27 +38,57 @@ Student::~Student()
     how_many_bought = 0 ;
     student_flavour = NULL;
     delete myCard;
-    curr = NULL;
+    curr_mach = NULL;
     student_printer->print(Printer::Student, student_id, 'F');
 }
 
+/*
+  purpose: returns a boolean value based on student's action 
+  precondition: None
+  postcondition: Boolean
+*/
 bool Student::action()
 {
-    if (how_many_bought == 0)
+    if (how_many_bought == how_many) // when student purchase everything in the list
     {
         return false;
     }
     
-    while (
-    
+    if (myCard->curr_balance() < curr_mach->cost()) // if there is insufficient fund, 
+    {
+        // continously transfer $3 to the card until we can buy at least one soda
+        while (myCard->curr_balance() < curr_mach->cost()) 
+        {
+            student_printer->print(Printer::Student, id, 't', 3);
+            student_office->transfer(id, 3, *myCard);
+            student_printer->print(Printer::Student, id, 'T', myCard->curr_balance());                         
+        }
+        return true;
+    }
+    // status when trying to purchase a soda
     VendingMachine::Status stock = curr->buy(student_flavour, myCard);
     
-    if (stock == 
-    
-    int rand_broken = prng(1,10);
-    
-    if (rand_broken == 1)
+    if (stock == BUY)      // student successfully bought a soda
     {
-    
+       how_many_bought += 1;
+       student_printer->print(Printer::Student, id, 'B',  myCard->curr_balance());
+       return true;
     }
+    else if (stock == STOCK)  // soda unavailable                  
+    {
+         // find another machine
+         curr_mach = student_server->getMachine(student_id);
+         return true;                                       
+    }
+    
+    if (myCard == NULL) // card destroyed by vending machine
+    {
+        student_printer->print(Printer::Student, id, 'D');
+        myCard = new WATCard(); 
+
+        student_printer->print(Printer::Student, id, 't', 5); 
+        student_office->transfer(id, 5, *myCard); // transfer $5 to empty card
+        student_printer->print(Printer::Student, id, 'T', myCard->curr_balance());     
+    }
+    
 }
