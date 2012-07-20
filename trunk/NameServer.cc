ca4bcf7235302246bebe54dfd78373f6b24ad5cd
y3046308@gmail.com <y3046308@gmail.com@e4ebeee5-115f-8226-58e5-ea274c292319>
2012-07-20 20:42:04 +0000

    fixed

--- trunk/NameServer.cc
+++ trunk/NameServer.cc
@@ -1,19 +1,6 @@
+#include "nameserver.h"
 
-#include<iostream>
-#include<string>
-#include<vector>
-#include"nameserver.h"
-#include"vendingmachine.h"
-
- //extern VendingMachine* machines;
-
-extern int currentStudentId;
-
-using namespace std;
-int NameServer::indexer;
- 
 NameServer::NameServer( Printer &prt, unsigned int numVendingMachines, unsigned int numStudents ){
-
 	printerptr = &prt;
 	numVM = numVendingMachines;
 	numStud = numStudents;
@@ -32,21 +19,24 @@ void NameServer::VMregister( VendingMachine *vendingmachine ){
  //cout << "REGISTERED : " << vendingmachine->getId() << endl;
 	printerptr->print(Printer::NameServer, 'R', vendingmachine->getId());
 	arrayOfMachines.at(counter) = vendingmachine;
-	counter = counter + 1;
+	counter += 1;
  }
  
 
 VendingMachine* NameServer::getMachine(unsigned int id){
-	
-	printerptr->print(Printer::NameServer, 'N', currentStudentId, arrayOfMachines.at((id+1) % numVM)->getId());
-	return arrayOfMachines.at((id + 1) % numVM); // returns the vending machine according to what the student should be assigned to.
+	printerptr->print(Printer::NameServer, 'N', id, arrayOfMachines.at(id % numVM)->getId());
+	return arrayOfMachines.at(id % numVM); // returns the vending machine according to what the student should be assigned to.
 }
 
 
 // Return a pointer to the vending machine array
- VendingMachine** NameServer::getMachineList(){
- 	VendingMachine** temp = &arrayOfMachines.at(0);
- 	return temp; // Return pointer to Vending Machine array, defined in driver.cc
+VendingMachine** NameServer::getMachineList(){
+  VendingMachine** temp = new VendingMachine*[numVM]; // create an arrray of 'numVM' pointers
+  for (unsigned int i = 0 ; i < numVM ; i += 1)
+  {
+     temp[i] = arrayOfMachines.at(i); // convert vector into a pointer to array
+  }
+  return temp; // Return pointer to Vending Machine array, defined in driver.cc
 
 }
 
