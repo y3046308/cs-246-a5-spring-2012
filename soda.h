d6956c80f59575afb4ae54819d26eb68c2f67491
y3046308 <y3046308@gmail.com>
2012-07-21 06:45:36 +0000

    a

---
+++ soda.h
@@ -0,0 +1,11 @@
+#ifndef __SODA_H__
+#define __SODA_H__
+#include "student.h"
+#include "WatCard.h"
+#include "office.h"
+#include "VendingMachine2.h"
+#include "NameServer.h"
+#include "plant.h"
+#include "truck.h"
+#include "printer.h"
+#endif
